752b2f432aa8575b063165ec1b9b8aabf5924576
iLaD08 <imadeddinelakhdar08@gmail.com>
2022-01-03 21:48:56 +0100

    fixed things up

--- api/utils/getPriceFeed.js
+++ api/utils/getPriceFeed.js
@@ -2,14 +2,21 @@ const axios = require("axios");
 
 module.exports = async (min, max) => {
   try {
-    const siteUrl = `https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=${min}&limit=${max}&sortBy=market_cap&sortType=desc&convert=USD&cryptoType=all&tagType=all&audited=false?aux=logo,urls,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap`;
+    const siteUrl = `https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=${min}&limit=${max}&sortBy=market_cap&sortType=desc&convert=USD,BTC,ETH&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap`;
 
     const { data } = await axios({
       method: "GET",
       url: siteUrl,
     });
 
-    return data.data.cryptoCurrencyList;
+    var newList = [];
+    data.data.cryptoCurrencyList.map(async (item) => {
+      item.logo = `https://s2.coinmarketcap.com/static/img/coins/64x64/${item.id}.png`;
+
+      await newList.push(item);
+    });
+
+    return newList;
   } catch (err) {
     return err;
   }
